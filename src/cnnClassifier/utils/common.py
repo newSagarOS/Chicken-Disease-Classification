caafa15b38888f7e0b8d02d62b48cea23965a796
Sagar Pandey <sagar.gkp.pandey@gmail.com>
2024-03-29 11:19:02 +0530

    Utils, logging and other files added

---
+++ src/cnnClassifier/utils/common.py
@@ -0,0 +1,99 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from cnnClassifier import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Read a YAML file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """  
+
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file:{path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml fileis empty")
+    except Exception as e:
+        raise e    
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """ Create list of directories
+
+    Args:
+        path_to_directories (list): list of directories to be created
+        ignore_log (bool, optional): ignore if multiple directories are to be created. Default is False.
+
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """Save a JSON data
+
+    Args:
+    path (Path): Path to json file
+    data (dict): data to be saved in json file
+    """
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"JSON file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+    Args:
+        path (Path): Path to json file
+    
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+    
+    logger.info(f"JSON file loaded successfully from: {path}")
+
+    return ConfigBox(content)
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """Get the size in kb
+    Args: 
+        path (Path): path of the file
+
+    Returns: 
+        str (size in kb)
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+
+    return f"~ {size_in_kb} KB"
+
+def decodeImage(imgstring, filename):
+    imgdata = base64.b64decode(imgstring)
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
